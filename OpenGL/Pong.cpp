ec474e44f84fef296fd5521295108eee01d1bb92
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-04-12 23:52:36 +0200

    Update Pong.cpp

--- OpenGL/Pong.cpp
+++ OpenGL/Pong.cpp
@@ -302,3 +302,22 @@ void draw(){
 	glutSwapBuffers();
 }
 
+int main (int argc, char ** argv){
+	srand(time(NULL));
+	settings.start_settings();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE);
+	glutInitWindowSize(settings.WinWid, settings.WinHei);
+	glutInitWindowPosition(settings.winXPos, settings.winYPos);
+	glutCreateWindow("***PONG***");
+	glutDisplayFunc(draw);
+	glutTimerFunc(settings.delay,Timer,0);
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboardUp);
+	glClearColor(0,0,0,1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-settings.OrthoWid,settings.OrthoWid,-settings.OrthoHei,settings.OrthoHei);
+	glutMainLoop();
+	return(0);
+}
