3d4bd4289781eafa6da94850b239192e52a072d3
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-04-15 11:38:29 +0200

    display Square

--- OpenGL/Moving Square.cpp
+++ OpenGL/Moving Square.cpp
@@ -1,3 +1,16 @@
 #include <stdio.h> 
 #include <GL/glut.h>
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(2.0, 4.0, 0.0);
+	glVertex3f(8.0, 4.0, 0.0);
+	glVertex3f(8.0, 6.0, 0.0);
+	glVertex3f(2.0, 6.0, 0.0);
+	glEnd();
+	glFlush();
+}
+
