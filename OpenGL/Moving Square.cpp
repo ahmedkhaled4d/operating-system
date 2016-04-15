0840df9c5981e95d476028bea3a3417afcc0e32b
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-04-15 13:58:11 +0200

    check X-Y axis

--- OpenGL/Moving Square.cpp
+++ OpenGL/Moving Square.cpp
@@ -3,16 +3,18 @@
 
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	//glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 0.0);
-	glBegin(GL_POLYGON);
-	glVertex3f(2.0, 4.0, 0.0);
-	glVertex3f(8.0, 4.0, 0.0);
-	glVertex3f(8.0, 6.0, 0.0);
-	glVertex3f(2.0, 6.0, 0.0);
+	glBegin(GL_POLYGON); // Drow Square
+		glVertex3f(2.0, 4.0, 0.0);
+		//glVertex3f(4.2, 4.2, .2);
+		glVertex3f(8.0, 4.0, 0.0);
+		glVertex3f(8.0, 8.0, 0.0);
+		glVertex3f(2.0, 8.0, 0.0);
 	glEnd();
 	glFlush();
 }
+
 
 int main(int argc, char **argv)
 {
@@ -28,10 +30,8 @@ int main(int argc, char **argv)
 	glMatrixMode(GL_PROJECTION);              // setup viewing projection 
 	glLoadIdentity();                           // start with identity matrix 
 	glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);   // setup a 10x10x2 viewing world
-
 	glutDisplayFunc(display);
 	glutMainLoop();
 
 	return 0;
 }
-
