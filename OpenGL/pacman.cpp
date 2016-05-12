57bf1b9279b4b4278e34f8325faff727d1001f12
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-05-12 14:36:09 +0200

    Type main Function
    	system("cls");
    	printf("You Lose and your score is : %i", pts);

--- OpenGL/pacman.cpp
+++ OpenGL/pacman.cpp
@@ -153,4 +153,109 @@ void FindPath(int sx, int sy, int x, int y){
 }
 
 
- 
+int main()
+{
+	bool running = true;
+	int x = 15; // hero x
+	int y = 16; // hero y
+	int old_x;
+	int old_y;
+
+	int ex = 1;
+	int ey = 1;
+
+
+	int pts = 0;
+
+	printf("Instruction:\n1. Arrow Keys to move your hero\n2. Eat the dots produced by the Eater to gain poins\n3. Don't get caught by the Eater\n\n");
+	printf("H -> Hard\nN -> Normal\nE -> Easy\n\nInput : ");
+
+	char diffi;
+	int speedmod = 3;
+
+	cin >> diffi;
+
+	if (diffi == 'N'){
+		speedmod = 2;
+	}
+	else if (diffi == 'H'){
+		speedmod = 1;
+	}
+
+	system("cls");
+	ShowMap();
+
+	gotoxy(x, y); cout << "H";
+
+	int frame = 0;
+
+	FindPath(ex, ey, x, y);
+
+	while (running){
+		gotoxy(x, y); cout << " ";
+
+		old_x = x;
+		old_y = y;
+
+		if (GetAsyncKeyState(VK_UP)){
+			if (map[y - 1][x] == '.'){ y--; pts++; }
+			else
+			if (map[y - 1][x] == ' ') y--;
+		}
+		if (GetAsyncKeyState(VK_DOWN)){
+			if (map[y + 1][x] == '.'){ y++; pts++; }
+			else
+			if (map[y + 1][x] == ' ') y++;
+		}
+		if (GetAsyncKeyState(VK_LEFT)){
+			if (map[y][x - 1] == '.'){ x--; pts++; }
+			else
+			if (map[y][x - 1] == ' ') x--;
+		}
+		if (GetAsyncKeyState(VK_RIGHT)){
+			if (map[y][x + 1] == '.'){ x++; pts++; }
+			else
+			if (map[y][x + 1] == ' ') x++;
+		}
+
+		if (old_x != x || old_y != y){
+			FindPath(ex, ey, x, y);
+		}
+
+		gotoxy(x, y); cout << "H";
+
+		map[ey][ex] = '.';
+		gotoxy(ex, ey); cout << ".";
+
+		if (frame%speedmod == 0 && walk_queue.size() != 0){
+			ex = walk_queue.back().x;
+			ey = walk_queue.back().y;
+			walk_queue.pop_back();
+		}
+
+		gotoxy(ex, ey); cout << "E";
+
+		if (ex == x && ey == y){
+			break;
+		}
+
+
+		gotoxy(32, 18);
+		gotoxy(32, 1); cout << pts;
+		Sleep(100);
+		frame++;
+	}
+
+	system("cls");
+	printf("You Lose and your score is : %i", pts);
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+
+	return 0;
+} 
